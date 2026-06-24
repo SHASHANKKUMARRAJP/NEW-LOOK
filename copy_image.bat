ddc1b7c5d198865d11526072ac6e9cfd216d36a9
SHASHANKKUMARRAJP <sshashankp682@gmail.com>
2026-06-24 17:12:33 +0530

    Update bridal companion image to traditional South Asian bridal suite

--- copy_image.bat
+++ copy_image.bat
@@ -11,6 +11,7 @@ copy "C:\Users\sshas\.gemini\antigravity-ide\brain\4f2052f4-ef77-4b7e-8ab8-9fc53
 copy "C:\Users\sshas\.gemini\antigravity-ide\brain\4f2052f4-ef77-4b7e-8ab8-9fc536328b17\gallery_nails_1782019877566.png" "%~dp0public\gallery_nails.png"
 copy "C:\Users\sshas\.gemini\antigravity-ide\brain\0b83d3f9-0d42-4fd2-8f18-238bae16c9b9\gallery_traditional_bride_1782300488912.png" "%~dp0public\gallery_traditional_bride.png"
 copy "C:\Users\sshas\.gemini\antigravity-ide\brain\0b83d3f9-0d42-4fd2-8f18-238bae16c9b9\bridal_glow_spa_1782301040651.png" "%~dp0public\service_pre_bridal_glow.png"
+copy "C:\Users\sshas\.gemini\antigravity-ide\brain\0b83d3f9-0d42-4fd2-8f18-238bae16c9b9\bridal_companion_traditional_1782301300780.png" "%~dp0public\service_bridal_companion.png"
 echo.
 echo Checking copied files...
 if exist "%~dp0public\premium_spa_hero.png" echo [SUCCESS] premium_spa_hero.png copied!
@@ -23,6 +24,7 @@ if exist "%~dp0public\gallery_makeup.png" echo [SUCCESS] gallery_makeup.png copi
 if exist "%~dp0public\gallery_nails.png" echo [SUCCESS] gallery_nails.png copied!
 if exist "%~dp0public\gallery_traditional_bride.png" echo [SUCCESS] gallery_traditional_bride.png copied!
 if exist "%~dp0public\service_pre_bridal_glow.png" echo [SUCCESS] service_pre_bridal_glow.png copied!
+if exist "%~dp0public\service_bridal_companion.png" echo [SUCCESS] service_bridal_companion.png copied!
 echo.
 pause
 
